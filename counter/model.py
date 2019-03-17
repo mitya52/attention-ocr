e6393b9613720e76f8b1a02b578a4d03f8b03ed7
mitya52 <dimitry.ageev@gmail.com>
2019-03-17 14:34:51 +0300

    tiny refactoring

--- counter/model.py
+++ counter/model.py
@@ -12,8 +12,8 @@ class ConvTower:
     def __init__(self,
                  inputs: tf.Tensor,
                  is_training: tf.Tensor):
-        self.inputs = inputs
-        self.is_training = is_training
+        self._inputs = inputs
+        self._is_training = is_training
 
     def _conv_block(self,
                     inputs: tf.Tensor,
@@ -25,12 +25,12 @@ class ConvTower:
             padding="same",
             activation=tf.nn.relu)
         pool = tf.layers.max_pooling2d(inputs=conv, pool_size=[2, 2], strides=2)
-        return tf.layers.dropout(inputs=pool, rate=0.2, training=self.is_training)
+        return tf.layers.dropout(inputs=pool, rate=0.2, training=self._is_training)
 
     def create(self,
                blocks: List[int],
                output_filters: Optional[int]) -> tf.Tensor:
-        x = self.inputs
+        x = self._inputs
         for filters in blocks:
             x = self._conv_block(x, filters)
 
@@ -51,41 +51,41 @@ class Attention:
                  feature_map: tf.Tensor,
                  sequences: tf.Tensor,
                  max_sequence_length: int):
-        self.feature_map = feature_map
-        self.sequences = sequences
-        self.max_sequence_length = max_sequence_length
+        self._feature_map = feature_map
+        self._sequences = sequences
+        self._max_sequence_length = max_sequence_length
 
-        self.num_units = 32
-        self.alphabet_size = 1
-        self.complete_alphabet_size = self.alphabet_size + 2
+        self._num_units = 32
+        self._alphabet_size = 1
+        self._complete_alphabet_size = self._alphabet_size + 2
 
         # special symbol codes
-        self.eos = self.alphabet_size
-        self.go = self.alphabet_size + 1
+        self._eos = self._alphabet_size
+        self._go = self._alphabet_size + 1
 
-        self.batch_size = array_ops.shape(self.feature_map)[0]
-        self.start_tokens = tf.ones([self.batch_size], dtype=tf.int32) * self.go
-        self.lengths = tf.reduce_sum(tf.to_int32(tf.not_equal(sequences, self.eos)), axis=1) + 1
+        self._batch_size = array_ops.shape(self._feature_map)[0]
+        self._start_tokens = tf.ones([self._batch_size], dtype=tf.int32) * self._go
+        self._lengths = tf.reduce_sum(tf.to_int32(tf.not_equal(sequences, self._eos)), axis=1) + 1
 
     def create_train_branch(self):
         # prepare sequences
-        sequences = tf.concat([tf.expand_dims(self.start_tokens, 1), self.sequences], axis=1)
-        sequences = slim.one_hot_encoding(sequences, num_classes=self.complete_alphabet_size)
+        sequences = tf.concat([tf.expand_dims(self._start_tokens, 1), self._sequences], axis=1)
+        sequences = slim.one_hot_encoding(sequences, num_classes=self._complete_alphabet_size)
 
         helper = seq2seq.TrainingHelper(
             inputs=sequences,
-            sequence_length=self.lengths)
+            sequence_length=self._lengths)
 
         outputs = self._decode(helper=helper, reuse=False)
 
         # complete logits to [batch_size, max_sequence_length, complete_alphabet_size]
         logits = outputs[0].rnn_output
-        to_complete = self.max_sequence_length - array_ops.shape(logits)[1]
+        to_complete = self._max_sequence_length - array_ops.shape(logits)[1]
 
         def complete_with_eos(logits, to_complete):
-            zeros_first = tf.zeros((self.batch_size, to_complete, self.alphabet_size), dtype=tf.float32)
-            zeros_last = tf.zeros((self.batch_size, to_complete, 1), dtype=tf.float32)
-            ones = tf.ones((self.batch_size, to_complete, 1), dtype=tf.float32)
+            zeros_first = tf.zeros((self._batch_size, to_complete, self._alphabet_size), dtype=tf.float32)
+            zeros_last = tf.zeros((self._batch_size, to_complete, 1), dtype=tf.float32)
+            ones = tf.ones((self._batch_size, to_complete, 1), dtype=tf.float32)
             completion = tf.concat([zeros_first, ones, zeros_last], axis=2)
             return tf.concat([logits, completion], axis=1)
 
@@ -98,9 +98,9 @@ class Attention:
 
     def create_predict_branch(self):
         helper = seq2seq.GreedyEmbeddingHelper(
-            embedding=lambda x: layers.one_hot_encoding(x, self.complete_alphabet_size),
-            start_tokens=self.start_tokens,
-            end_token=self.eos)
+            embedding=lambda x: layers.one_hot_encoding(x, self._complete_alphabet_size),
+            start_tokens=self._start_tokens,
+            end_token=self._eos)
 
         outputs = self._decode(helper=helper, reuse=True)
 
@@ -113,23 +113,23 @@ class Attention:
         NOTE: this is experimental state creation from feature map, but works better than zero_state
         TODO: check helpfulness of birnn (may be only dense layer needed)
         '''
-        cell_fw = tf.nn.rnn_cell.GRUCell(num_units=self.num_units,
+        cell_fw = tf.nn.rnn_cell.GRUCell(num_units=self._num_units,
                                          reuse=reuse,
                                          name='{}_cell_fw'.format(prefix))
-        cell_bw = tf.nn.rnn_cell.GRUCell(num_units=self.num_units,
+        cell_bw = tf.nn.rnn_cell.GRUCell(num_units=self._num_units,
                                          reuse=reuse,
                                          name='{}_cell_bw'.format(prefix))
-        x = tf.unstack(self.feature_map, axis=1)
+        x = tf.unstack(self._feature_map, axis=1)
         outputs, _, _ = rnn.static_bidirectional_rnn(
             inputs=x,
             cell_fw=cell_fw,
             cell_bw=cell_bw,
-            initial_state_fw=cell_fw.zero_state(self.batch_size, dtype=tf.float32),
-            initial_state_bw=cell_bw.zero_state(self.batch_size, dtype=tf.float32),
+            initial_state_fw=cell_fw.zero_state(self._batch_size, dtype=tf.float32),
+            initial_state_bw=cell_bw.zero_state(self._batch_size, dtype=tf.float32),
             dtype=tf.float32)
         x = tf.stack(outputs, axis=1)
         x = tf.layers.dense(inputs=x,
-                            units=self.num_units,
+                            units=self._num_units,
                             activation='relu',
                             name='{}_dense_state'.format(prefix))
         return tf.reduce_mean(input_tensor=x, axis=1)
@@ -138,7 +138,7 @@ class Attention:
                        cell: rnn.OutputProjectionWrapper,
                        reuse: bool,
                        create_state: bool = False) -> seq2seq.AttentionWrapperState:
-        state = cell.zero_state(self.batch_size, dtype=tf.float32)
+        state = cell.zero_state(self._batch_size, dtype=tf.float32)
         if create_state:
             state = state.clone(cell_state=self._create_state(reuse=reuse, prefix='s'))
         return state
@@ -148,10 +148,10 @@ class Attention:
                 reuse: bool = False):
         with tf.variable_scope('decoder', reuse=reuse):
             attention_mechanism = seq2seq.BahdanauAttention(
-                num_units=self.num_units,
-                memory=self.feature_map)
+                num_units=self._num_units,
+                memory=self._feature_map)
             rnn_cell = tf.nn.rnn_cell.GRUCell(
-                num_units=self.num_units)
+                num_units=self._num_units)
             attention_cell = seq2seq.AttentionWrapper(
                 cell=rnn_cell,
                 attention_mechanism=attention_mechanism,
@@ -159,7 +159,7 @@ class Attention:
 
             output_cell = rnn.OutputProjectionWrapper(
                 cell=attention_cell,
-                output_size=self.complete_alphabet_size,
+                output_size=self._complete_alphabet_size,
                 reuse=reuse)
             decoder = tf.contrib.seq2seq.BasicDecoder(
                 cell=output_cell,
@@ -170,7 +170,7 @@ class Attention:
                 decoder=decoder,
                 output_time_major=False,
                 impute_finished=True,
-                maximum_iterations=self.max_sequence_length)
+                maximum_iterations=self._max_sequence_length)
 
         return outputs
 
@@ -225,7 +225,7 @@ class Model:
     def _create_loss(logits: tf.Tensor,
                      sequences_input: tf.Tensor,
                      attention_model: Attention) -> tf.Tensor:
-        weights = tf.sequence_mask(attention_model.lengths, attention_model.max_sequence_length)
+        weights = tf.sequence_mask(attention_model._lengths, attention_model._max_sequence_length)
         return seq2seq.sequence_loss(
             logits=logits,
             targets=sequences_input,
@@ -235,7 +235,7 @@ class Model:
     def _alignments_mean(alignments: tf.Tensor,
                          ct_out_shape: tf.TensorShape):
         alignments = tf.reduce_mean(alignments, axis=0, keep_dims=False)
-        alignments = tf.reshape(alignments, [-1, ct_out_shape[1], ct_out_shape[2]])
+        alignments = tf.reshape(alignments, [-1, ct_out_shape[1], ct_out_shape[2], 1])
         return alignments
 
     @staticmethod
@@ -243,7 +243,7 @@ class Model:
                          ct_out_shape: tf.TensorShape):
         depth = array_ops.shape(alignments)[0]
         alignments = tf.transpose(alignments, [1, 0, 2])
-        alignments = tf.reshape(alignments, [-1, depth, ct_out_shape[1], ct_out_shape[2]])
+        alignments = tf.reshape(alignments, [-1, ct_out_shape[1], ct_out_shape[2], depth])
         return alignments
 
 
